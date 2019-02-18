499378c2afdc5061cadd1c28c93c6406bb2d0001
Daniel <daniel.keighley@wpengine.com>
2019-02-18 11:25:21 +0000

    Add GameManager, LevelManager and Loader

---
+++ WPGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    private LevelManager levelScript;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        levelScript = GetComponent<LevelManager>();
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        levelScript.LevelInit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
