6e68885604f1c17a854bbed9a3855fbf08fffcbf
Daniel <daniel.keighley@wpengine.com>
2019-02-18 11:47:47 +0000

    Set up GameManager and Main Camera prefabs.

--- WPGame/Assets/Scripts/Loader.cs
+++ WPGame/Assets/Scripts/Loader.cs
@@ -6,12 +6,17 @@ public class Loader : MonoBehaviour
 {
 
     public GameObject gameManager;
+    public GameObject soundManager;
 
     // Use this for initialization
     void Awake()
     {
 
         if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+        if (SoundManager.instance == null)
         {
             Instantiate(gameManager);
         }
---
+++ WPGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager instance;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
